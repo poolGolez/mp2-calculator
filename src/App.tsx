0be27cabbf382d600697fee89265a69a72fbc910
Pool Golez <loop.edward@gmail.com>
2025-12-24 21:12:33 +0800

    Add EarningSection component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Box, Divider, Stack, Typography } from "@mui/material";
+import { Box, Divider, Stack } from "@mui/material";
 import ContributionsTable from "./components/ContributionsTable";
 import { useMemo, useState } from "react";
 import ContributionsInputForm from "./components/ContributionsInputForm";
-import { formatNumber } from "./utils/numeric";
 import Mp2Calculation from "./services/calculator";
+import EarningsSection from "./components/EarningsSection";
 
 function App() {
   const initialInterestRate = 6.65;
@@ -21,14 +21,8 @@ function App() {
     return new Mp2Calculation(contributions, interestRates);
   }, [contributions, interestRates]);
 
-  const { totalDividends, totalContributionAmount, endingBalance } =
-    calculation;
-
-  const totalDividendsPercent =
-    (calculation.totalDividends / calculation.totalContributionAmount) * 100;
-
   return (
-    <Stack sx={{ height: "100vh" }} spacing={2}>
+    <Stack sx={{ height: "100vh", width: "100%" }} spacing={2}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -42,46 +36,20 @@ function App() {
         spacing={2}
         divider={<Divider orientation="vertical" flexItem />}
       >
-        <ContributionsInputForm
-          initState={{
-            startingContribution: 500.0,
-            monthlyContribution: 500.0,
-            estimatedRate: initialInterestRate,
-          }}
-          onUpdate={setContributions}
-          setInterestRates={setInterestRates}
-        />
-        <Stack spacing={2} flex={1}>
-          <Stack direction="row" spacing={1}>
-            <Box flex={1} textAlign="left">
-              Total Dividends Earned
-            </Box>
-            <Box flex={1} textAlign="right" fontWeight="bold">
-              <Typography>
-                <Typography component="span" fontWeight="bold">
-                  {formatNumber(totalDividends)}
-                </Typography>
-                {` (${formatNumber(totalDividendsPercent)}%)`}
-              </Typography>
-            </Box>
-          </Stack>
-          <Stack direction="row" spacing={1}>
-            <Box flex={1} textAlign="left">
-              Total Contributions
-            </Box>
-            <Box flex={1} textAlign="right" fontWeight="bold">
-              {formatNumber(totalContributionAmount)}
-            </Box>
-          </Stack>
-          <Stack direction="row" spacing={1}>
-            <Box flex={1} textAlign="left">
-              Ending Balance (after 5 years)
-            </Box>
-            <Box flex={1} textAlign="right" fontWeight="bold">
-              {formatNumber(endingBalance)}
-            </Box>
-          </Stack>
-        </Stack>
+        <Box flex={2}>
+          <ContributionsInputForm
+            initState={{
+              startingContribution: 500.0,
+              monthlyContribution: 500.0,
+              estimatedRate: initialInterestRate,
+            }}
+            onUpdate={setContributions}
+            setInterestRates={setInterestRates}
+          />
+        </Box>
+        <Box flex={1}>
+          <EarningsSection calculation={calculation} />
+        </Box>
       </Stack>
       <ContributionsTable calculation={calculation} />
     </Stack>
---
+++ src/components/EarningsSection.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { formatNumber } from "../utils/numeric";
+import type Mp2Calculation from "../services/calculator";
+
+interface EarningsSectionProps {
+  calculation: Mp2Calculation;
+}
+
+const EarningsSection: React.FC<EarningsSectionProps> = ({
+  calculation: { totalDividends, totalContributionAmount, endingBalance },
+}) => {
+  const totalDividendsPercent =
+    (totalDividends / totalContributionAmount) * 100;
+
+  return (
+    <Stack spacing={2} flex={1}>
+      <Stack direction="row" spacing={1}>
+        <Box flex={1} textAlign="left">
+          Total Dividends Earned
+        </Box>
+        <Box flex={1} textAlign="right" fontWeight="bold">
+          <Typography>
+            <Typography component="span" fontWeight="bold">
+              {formatNumber(totalDividends)}
+            </Typography>
+            {` (${formatNumber(totalDividendsPercent)}%)`}
+          </Typography>
+        </Box>
+      </Stack>
+      <Stack direction="row" spacing={1}>
+        <Box flex={1} textAlign="left">
+          Total Contributions
+        </Box>
+        <Box flex={1} textAlign="right" fontWeight="bold">
+          {formatNumber(totalContributionAmount)}
+        </Box>
+      </Stack>
+      <Stack direction="row" spacing={1}>
+        <Box flex={1} textAlign="left">
+          Ending Balance (after 5 years)
+        </Box>
+        <Box flex={1} textAlign="right" fontWeight="bold">
+          {formatNumber(endingBalance)}
+        </Box>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default EarningsSection;
